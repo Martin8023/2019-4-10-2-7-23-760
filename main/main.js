416e8a94bc85066afdbedbb43c147290419f4c77
martin <martin802399@outlook.com>
2019-05-25 14:42:52 +0800

    done task

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    if (inputs == null) return null;
+    if (inputs.distance == null) return null;
+    if (inputs.distance == 0)return null;
+
+    price1 = runPrice(inputs.distance);
+    price2 = 0;
+
+    if (inputs.parkTime != null && inputs.parkTime != 0){
+        price2 = parkPrice(inputs.parkTime);
+    }
+
+    price = Math.round(price1+price2);
+    console.log(price1+" "+price2+" "+price);
+    return price;
 };
+
+function runPrice(distance) {
+    if (distance <= 2){
+        return 6;
+    }
+    else if (distance <= 8) {
+        return 6+(distance-2)*0.8;
+    }else {
+        return 10.8+(distance-8)*1.2;
+    }
+}
+
+function parkPrice(min) {
+    return 0.25*min;
+}
